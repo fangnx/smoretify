733ec2e9c389a18dd69b76f386bde2e6c4818ad3
fangnx <fangnaxin@gmail.com>
2019-09-07 18:34:08 -0400

    Removed config folder

--- config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  host: process.env.HOST || '127.0.0.1',
-  port: process.env.PORT || 8888
-};
--- config/geniusAuthInfo.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  ACCESS_TOKEN:
-    'KGQ7OO-SYtejw2vwMVPmSG6xPxy4tmLaMN0jnkBygC3A2GFA7yCA2gM1kwsEQxpb'
-};
